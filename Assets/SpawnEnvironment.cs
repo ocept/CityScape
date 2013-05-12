aaef171baa39d6e9409557f670e71a9ae865533c
Oliver Warden <oliverwarden@gmail.com>
2013-05-12 20:23:41 +0100

    Only spawn towers once, fix positioning

--- Assets/SpawnEnvironment.cs
+++ Assets/SpawnEnvironment.cs
@@ -14,27 +14,26 @@ public class SpawnEnvironment : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		spawnSurroundings(); //TODO: should only be called when camera has moved into new area
+	}
+	void spawnSurroundings()
+	{
 		float kx = gameObject.transform.localPosition.x;
 		float kz = gameObject.transform.localPosition.z;
 		int ikx = Mathf.FloorToInt(kx/100)*100;
-		int ikz = Mathf.FloorToInt(kx/100)*100;
-		spawnScraper(ikx + 20,ikz);
+		int ikz = Mathf.FloorToInt(kz/100)*100;
+		if(GameObject.Find((ikx + 20).ToString()+","+ikz.ToString())== null){
+			spawnScraper(ikx + 20,ikz);
+		}
 	}
 	void spawnScraper(int x, int z)
 	{
 		Vector3 spawnPos = new Vector3(x, 20, z);
 		int heightVar = ScraperStore.getHeight(x, z);
-		
+
 		scraper.transform.localScale = new Vector3(25,100 + heightVar, 25);
 		spawnPos.y += heightVar/2;
-		Instantiate(scraper, spawnPos, Quaternion.identity);
-	}
-	void spawnScraper()
-	{
-		Vector3 spawnPos = new Vector3(250, 20, -100) + new Vector3(Random.Range(-50, 50), 0 , Random.Range(-50, 50));
-		int heightVar = Random.Range(-20, 50);
-		scraper.transform.localScale = new Vector3(25,100 + heightVar, 25);
-		spawnPos.y += heightVar/2;
-		Instantiate(scraper, spawnPos, Quaternion.identity);
+		var scrape = Instantiate(scraper, spawnPos, Quaternion.identity);
+		scrape.name = (x.ToString()+","+z.ToString());
 	}
 }
